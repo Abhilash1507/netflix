3387cd6411d54e0159740144fb0f753b09731cf2
Abhilash <abhilashvm56@gmail.com>
2022-05-27 13:13:33 +0530

    dummy data added on posters

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import NavBar from "./Components/Navbar/NavBar";
 import "./App.css"
 import Banner from "./Components/Banner/Banner";
+import RowPost from "./Components/RowPost/RowPost";
 
 function App() {
   return (
     <div className="App">
          
         <NavBar/>    
-        <Banner/>   
+        <Banner/>  
+        <RowPost/> 
     </div>
   );
 }
