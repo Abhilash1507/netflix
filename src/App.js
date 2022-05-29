6b35e2356a9756f33d22deb0777e85ea2775e93f
Abhilash <abhilashvm56@gmail.com>
2022-05-29 12:15:42 +0530

    movie trailer play added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from "./Components/Navbar/NavBar";
 import "./App.css"
 import Banner from "./Components/Banner/Banner";
 import RowPost from "./Components/RowPost/RowPost";
+import { action, comedy,  Horror, Orginals, romance } from './urls'
 
 function App() {
   return (
@@ -10,9 +11,15 @@ function App() {
          
         <NavBar/>    
         <Banner/>  
-        <RowPost/> 
+        <RowPost url = {Orginals} title ="Netflix Orginals"/> 
+        <RowPost url = {Horror} title = "Horror" isSmall /> 
+        <RowPost url = {action} title = "Action" isSmall />
+        <RowPost url = {comedy} title = "Comedy" isSmall />
+         <RowPost url = {romance} title = "Romance" isSmall /> 
+
     </div>
   );
+
 }
 
 export default App;
--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -1,14 +1,16 @@
-import React, { useEffect, useState } from 'react'
-import './RowPost.css'
-import axios  from "../../Axios"
-import {API_KEY,imageUrl} from "../../Constants/Constants"
-function RowPost() {
+import React, { useEffect, useState } from 'react';
+import './RowPost.css';
+import axios  from "../../Axios";
+import {API_KEY, imageUrl} from "../../Constants/Constants";
+import YouTube from 'react-youtube';
+function RowPost(props) {
  
   const [movie, setMovie] = useState([])
+  const [urlId, setUrlID] = useState('')
 
  useEffect(() => {
    
-   axios.get(`discover/tv?api_key=${API_KEY}&with_networks=213`).then((response)=>{
+   axios.get(props.url).then((response)=>{
      console.log(response.data)
      setMovie(response.data.results);
    }).catch(err=>{
@@ -16,23 +18,44 @@ function RowPost() {
    })
   
  }, [])
- 
+
+ const opts = {
+  height: '390',
+  width: '100%',
+  playerVars: {
+    // https://developers.google.com/youtube/player_parameters
+    autoplay: 1,
+  }
+  };
+   
+  const movieTrailor=(id)=>{
+    console.log(id);
+    axios.get(`movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then((response)=>{
+        if(response.data.results.length !==0){
+            setUrlID(response.data.results[0])
+        }else{
+         alert('Trailer not found..')
+        }
+    })
+  }
 
   return (
     <div className='row'>
-        <h2>Netflix Orginals</h2>
+        <h2>{props.title}</h2>
         <div className='posters' >
           
           {
             movie.map((obj)=>
            
-              <img className='poster' src={`${imageUrl+obj.backdrop_path}`} alt="" />
+              <img onClick={()=>movieTrailor(obj.id)} className={props.isSmall ? 'smallPoster': 'poster'} src={`${imageUrl+obj.backdrop_path}`} alt="" />
 
             )
           }
        
                 
         </div>
+       {  urlId && <YouTube opts={opts} videoId={urlId.key}/>}
+
 
     </div>
   )
